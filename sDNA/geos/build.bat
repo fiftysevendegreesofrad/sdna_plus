2501d3bbc231f2dc42dd070cb262bc45b7f79b39
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2023-11-10 12:16:53 +0000

    Don't build x86 geos on x64 only branch

--- sDNA/geos/build.bat
+++ sDNA/geos/build.bat
@@ -1,4 +1,4 @@
-xcopy drop x86\ /E
+@REM xcopy drop x86\ /E
 xcopy drop x64\ /E
 
 rem path must be set to include c:\Program Files\Microsoft Visual Studio 9.0\VC\bin
@@ -11,11 +11,11 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\Vcvarsall.bat" amd64
 nmake /f makefile.vc MSVC_VER=1500
 cd ..
 
-cd x86
-call autogen.bat
+@REM cd x86
+@REM call autogen.bat
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\Vcvarsall.bat" x86
+@REM call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\Vcvarsall.bat" x86
 
-nmake /f makefile.vc MSVC_VER=1500
-cd ..
+@REM nmake /f makefile.vc MSVC_VER=1500
+@REM cd ..
 
