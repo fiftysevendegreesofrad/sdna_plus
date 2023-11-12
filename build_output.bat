b78c9946300491ee64123593eda301763c46c84a
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2023-11-12 13:48:28 +0000

    Rename .proj files to .vcproj

--- build_output.bat
+++ build_output.bat
@@ -1,5 +1,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall" amd64
 
-c:\windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe build_output.proj /t:rebuild /p:Configuration=release
+c:\windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe build_output.vcproj /t:rebuild /p:Configuration=release
 
 pause
--- build_release.bat
+++ build_release.bat
@@ -1,5 +1,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall" amd64
 
-c:\windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe build_installer.proj /t:rebuild /p:Configuration=release
+c:\windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe build_installer.vcproj /t:rebuild /p:Configuration=release
 
 pause
