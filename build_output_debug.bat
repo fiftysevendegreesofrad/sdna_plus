6a853546b12790ee2e6cd1ba0e9bbc45febeb79b
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2023-11-12 13:53:54 +0000

    build_{installer,output}.vcproj -> .proj

--- build_output_debug.bat
+++ build_output_debug.bat
@@ -1,5 +1,5 @@
 call vcvars64
 
-c:\windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe build_output.vcproj /t:rebuild /p:Configuration=debug /m
+c:\windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe build_output.proj /t:rebuild /p:Configuration=debug /m
 
 pause
