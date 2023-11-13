a0c52d76668f6ffc759b41f8b30569389074ac54
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2023-11-13 22:17:27 +0000

    Specify vcvarsall.bat

--- build_release.bat
+++ build_release.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall" amd64
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 
 c:\windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe build_installer.proj /t:rebuild /p:Configuration=release
 
