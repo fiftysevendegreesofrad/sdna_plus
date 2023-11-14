854203b2ba495f7ae159616cd1f6f56780c48d13
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2023-11-14 15:59:49 +0000

    Bump version of MSBuild.exe to fix batch files building the .proj files
    There's an x64 version of MSBuild.exe too on my machine.  Probably unnecessary.

--- build_output.bat
+++ build_output.bat
@@ -1,5 +1,6 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall" amd64
 
-c:\windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe build_output.proj /t:rebuild /p:Configuration=release
+@REM c:\windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe build_output.proj /t:rebuild /p:Configuration=release
+"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" /t:rebuild /p:Configuration=release
 
 pause
