89690f8dcc954560a34f091ca1b53f2df2f51d4b
Crispin <cooperch@cardiff.ac.uk>
2023-12-20 13:46:08 +0000

    mostly working test suite, when called with python3exe="C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" python2exe=c:\Python27\ArcGISx6410.8\python.exe sdnadll=C:\code\sdna_plus_crispin\output\Debug\x64\sdna_vs2008.dll except for some minor oddities that are bugs in tests

--- sDNA/sdna_vs2008/tests/test_parallel_results.py
+++ sDNA/sdna_vs2008/tests/test_parallel_results.py
@@ -19,9 +19,7 @@ class my_void_p(ctypes.c_void_p):
 
 serial_dll_path = os.environ["sdnadll"]
 parallel_dll_path = re.sub("debug","parallel_debug",serial_dll_path,flags=re.IGNORECASE)
-abs_ser_dll_path =os.path.dirname(os.path.abspath(__file__))+os.path.sep+serial_dll_path
-eprint ("sdnapy dll path",abs_ser_dll_path)
-sdnapy.set_dll_path(abs_ser_dll_path)
+sdnapy.set_dll_path(serial_dll_path)
 
 if os.path.getmtime(serial_dll_path)-os.path.getmtime(parallel_dll_path) > 3600*10:
     eprint ("dlls built more than 10 hours apart")
