e038822e0e9760f3ff2b224e6bac10b543d01ce1
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-02-18 18:49:16 +0000

    Support using Python 3 as the build Python.  Fix regex.

--- getSdnaVersion.py
+++ getSdnaVersion.py
@@ -4,7 +4,7 @@ def getVersion():
     version = None
     versionfile = os.path.dirname(__file__)+os.sep+"sDNA"+os.sep+"sdna_vs2008"+os.sep+"version_template.h"
     for line in (open (versionfile, 'rt')):
-        m = re.match('^const char \*SDNA_VERSION = "(.*)";.*',line)
+        m = re.match(r'^const char \*SDNA_VERSION = "(.*)";.*',line)
         if m:
             assert (version==None)
             version = m.group(1)
--- installerbits/_parentdir.py
+++ installerbits/_parentdir.py
@@ -1,13 +1,16 @@
 import sys,os
 
-try:
-    unicode
-except NameError:
-    unicode = str 
+#  On Python 2 and earlier
+if sys.version_info.major <= 2:
+    encoding = sys.getfilesystemencoding()
+    path = os.path.dirname(unicode(__file__, encoding))
+else: 
+      # On Python 3 there is (and it can be assumed 
+      # in all future versions there will be) 
+      # native unicode support
+    path = os.path.dirname(__file__)
 
 
-encoding = sys.getfilesystemencoding()
-path = os.path.dirname(unicode(__file__, encoding))
 parentdir = os.path.dirname(path)
 if parentdir not in sys.path:
     sys.path.insert(0,parentdir)
