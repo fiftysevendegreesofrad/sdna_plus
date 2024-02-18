4bfb074eef24863d20f202487811e938e7efb55c
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-02-18 16:42:57 +0000

    Make rename_version.py Python 3 compatible, and specify file opener code in getSdnaVersion.py

--- getSdnaVersion.py
+++ getSdnaVersion.py
@@ -3,7 +3,7 @@ import re,os
 def getVersion():
     version = None
     versionfile = os.path.dirname(__file__)+os.sep+"sDNA"+os.sep+"sdna_vs2008"+os.sep+"version_template.h"
-    for line in (open (versionfile)):
+    for line in (open (versionfile, 'rt')):
         m = re.match('^const char \*SDNA_VERSION = "(.*)";.*',line)
         if m:
             assert (version==None)
--- installerbits/rename_version.py
+++ installerbits/rename_version.py
@@ -1,14 +1,16 @@
+from __future__ import print_function
+
 import os,re,sys,_parentdir,shutil
 from getSdnaVersion import getVersion
 
 installfile,outputdir = sys.argv[1:3]
 
-print installfile
-print outputdir
+print(installfile)
+print(outputdir)
 
 version = getVersion()
 
-filename_friendly_version = re.sub("\.","_",version)
+filename_friendly_version = re.sub(r"\.","_",version)
 
 outfilename = "%s/sDNA_setup_win_v%s.msi"%(outputdir,filename_friendly_version)
 
