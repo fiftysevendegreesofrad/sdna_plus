b676b422571c4700d70ab7d4d12946d4c16db33e
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-06-01 22:41:41 +0100

    Update create_build_system.bat

--- sDNA/sdna_vs2008/create_build_system.bat
+++ sDNA/sdna_vs2008/create_build_system.bat
@@ -3,4 +3,4 @@ set THIS_FILE_DIR=%~dp0
 set SRC_DIR=%THIS_FILE_DIR%
 set BUILD_DIR=%THIS_FILE_DIR%\..\..\build_cmake
 mkdir %BUILD_DIR%
-cmake -G %BUILD_DIR% -S %SRC_DIR%
+cmake -B %BUILD_DIR% -S %SRC_DIR%
