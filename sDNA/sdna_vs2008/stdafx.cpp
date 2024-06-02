8f274f2e88a3774f6b26af2795eeee6c8566222f
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-06-02 23:30:52 +0100

    Don't compile stdafx_force_includable.cpp when using MSBuild, only via CMake

--- sDNA/sdna_vs2008/stdafx.cpp
+++ sDNA/sdna_vs2008/stdafx.cpp
@@ -1,1 +1,9 @@
+// stdafx.cpp : source file that includes just the standard includes
+// sDNA.pch will be the pre-compiled header (cmake_pch.pch or .obj if using CMake)
+// stdafx.obj will contain the pre-compiled type information (cmake_pch.obj if using CMake)
+
+
 #include "stdafx.h"
+
+// TODO: reference any additional headers you need in stdafx_force_includable.h
+// and not in this file
--- sDNA/sdna_vs2008/stdafx_force_includable.cpp
+++ sDNA/sdna_vs2008/stdafx_force_includable.cpp
@@ -1,10 +1,4 @@
-// stdafx_force_includable.cpp (used to  be stdafx.cpp) : source file that includes just the standard includes
-// sDNA.pch will be the pre-compiled header (cmake_pch.pch or .obj if using CMake)
-// stdafx_force_includable.obj (used to be stdafx.obj) will contain the pre-compiled type information
+// This file is only compiled when using CMake
 
-#ifndef FORCE_INCLUDE_PRECOMPILED_HEADER
 #include "stdafx_force_includable.h"
-#endif
 
-// TODO: reference any additional headers you need in stdafx_force_includable.h
-// and not in this file
