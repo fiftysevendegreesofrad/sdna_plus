aa6a55cfa835cadb5130046c0c9fd5b28bc1cd15
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-06-02 21:43:08 +0100

    Refactor to use stdafx_force_includable.h

---
+++ sDNA/sdna_vs2008/stdafx_force_includable.cpp
@@ -0,0 +1,10 @@
+// stdafx_force_includable.cpp (used to  be stdafx.cpp) : source file that includes just the standard includes
+// sDNA.pch will be the pre-compiled header (cmake_pch.pch or .obj if using CMake)
+// stdafx_force_includable.obj (used to be stdafx.obj) will contain the pre-compiled type information
+
+#ifndef FORCE_INCLUDE_PRECOMPILED_HEADER
+#include "stdafx_force_includable.h"
+#endif
+
+// TODO: reference any additional headers you need in stdafx_force_includable.h
+// and not in this file
