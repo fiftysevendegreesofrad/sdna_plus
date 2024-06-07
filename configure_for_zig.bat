e4b2615acdd544a36b04a51279ddca6bdae1cdb4
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-06-07 14:26:26 +0100

    Configures!

--- configure_for_zig.bat
+++ configure_for_zig.bat
@@ -6,8 +6,6 @@ set SRC_DIR=%THIS_FILE_DIR%
 
 set BUILD_DIR=%THIS_FILE_DIR%\build_output_cmake_zig
 
-cmake -D CMAKE_CXX_COMPILER="zig cc" ^
-      -D CMAKE_C_COMPILER="zig c++" ^
-      -G Ninja ^
+cmake -G Ninja ^
       -B %BUILD_DIR% ^
       -S %SRC_DIR%
