4843a7089f2ce6520439987e3919dd6d31049571
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-06-07 13:41:18 +0100

    Add zigcross

--- configure_for_zig.bat
+++ configure_for_zig.bat
@@ -6,7 +6,8 @@ set SRC_DIR=%THIS_FILE_DIR%
 
 set BUILD_DIR=%THIS_FILE_DIR%\build_output_cmake_zig
 
-cmake -D CMAKE_CXX_COMPILER="C:/ProgramData/chocolatey/lib/zig/tools/zig-windows-x86_64-0.11.0/zig.exe" ^
+cmake -D CMAKE_CXX_COMPILER="zig cc" ^
+      -D CMAKE_C_COMPILER="zig c++" ^
       -G Ninja ^
       -B %BUILD_DIR% ^
       -S %SRC_DIR%
