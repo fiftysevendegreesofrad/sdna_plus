2e7a60492c77a4247583d304b77ba24c1c1f893f
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-06-09 14:23:26 +0100

    Use same batch file as the VS CMake build

--- create_installer_with_CMake.bat
+++ create_installer_with_CMake.bat
@@ -1,6 +1,14 @@
-.\configure_output_build_system_with_CMake.bat Win32 && ^
+setlocal
+
+if [%~1]==[] (
+    set GENERATOR="Visual Studio 17 2022"
+) else (
+    set GENERATOR=%~1
+)
+
+.\configure_output_build_system_with_CMake.bat Win32 %GENERATOR% && ^
 .\run_CMake_output_build_system.bat && ^
-.\configure_output_build_system_with_CMake.bat x64 && ^
+.\configure_output_build_system_with_CMake.bat x64 %GENERATOR% && ^
 .\run_CMake_output_build_system.bat && ^
 AdvancedInstaller.com /build installerbits\advanced\sdna.aip && ^
 python -u installerbits\rename_version.py installerbits/advanced/output/sdna_setup.msi .
