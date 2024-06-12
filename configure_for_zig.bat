ea27d94a22ed9c6c0953c31524e07c9276e13bea
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-06-12 19:23:17 +0100

    Copy in changes from Zig branch

--- configure_for_zig.bat
+++ configure_for_zig.bat
@@ -1,4 +1,7 @@
 @echo off
+@REM Don't use setlocal.  Subsequent calls of 
+@REM ./run_CMake_output_build_system.bat use
+@REM the value of %BUILD_DIR% set below.
 
 set THIS_FILE_DIR=%~dp0
 set SRC_DIR=%THIS_FILE_DIR%
@@ -6,7 +9,6 @@ set SRC_DIR=%THIS_FILE_DIR%
 
 set BUILD_DIR=%THIS_FILE_DIR%\build_output_cmake_zig
 
-cmake -G Ninja ^
+cmake -G "Ninja Multi-Config" ^
       -B %BUILD_DIR% ^
-      -S %SRC_DIR% ^
-      -D USE_ZIG=ON
+      -S %SRC_DIR%
--- create_installer_with_CMake.bat
+++ create_installer_with_CMake.bat
@@ -1,6 +1,15 @@
-.\configure_output_build_system_with_CMake.bat Win32 && ^
-.\run_CMake_output_build_system.bat && ^
-.\configure_output_build_system_with_CMake.bat x64 && ^
-.\run_CMake_output_build_system.bat && ^
+setlocal
+
+if [%~1]==[] (
+    @REM set GENERATOR="Visual Studio 17 2022"
+    set GENERATOR="Ninja Multi-Config"
+) else (
+    set GENERATOR=%~1
+)
+
+.\configure_output_build_system_with_CMake.bat Win32 %GENERATOR% && ^
+.\run_CMake_output_build_system.bat Release && ^
+.\configure_output_build_system_with_CMake.bat x64 %GENERATOR% && ^
+.\run_CMake_output_build_system.bat Release && ^
 AdvancedInstaller.com /build installerbits\advanced\sdna.aip && ^
 python -u installerbits\rename_version.py installerbits/advanced/output/sdna_setup.msi .
