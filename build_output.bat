ff44843d69537fb8876ca3f5d1c26667eea24550
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-06-14 22:27:49 +0100

    Update build_output.bat

--- build_output.bat
+++ build_output.bat
@@ -1,6 +1,6 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall" amd64
 
 @REM c:\windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe build_output.proj /t:rebuild /p:Configuration=release
-"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" /t:rebuild /p:Configuration=release
+"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" build_installer.proj /t:rebuild /p:Configuration=release /p:Platform=x64 /p:VcpkgEnableManifest=true
 
 pause
