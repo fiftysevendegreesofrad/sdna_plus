9b0b9f5cd9c146f53faa87d6cbeeb8ae343d7f9e
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-06-18 18:20:06 +0100

    Let arcscriptsdir find arcscripts on Linux. Remove test that needs numpy from smoke tests

--- sDNA/sdna_vs2008/tests/arcscriptsdir.py
+++ sDNA/sdna_vs2008/tests/arcscriptsdir.py
@@ -1,7 +1,19 @@
 import sys,os
+
+
 up = os.path.dirname
 
-path = up(__file__)
-arcscriptsdir = up(up(up(path)))+r"\arcscripts"
+sdna_dll = os.getenv('sdnadll','')
+
+arcscriptsdir = ''
+
+if sdna_dll:
+    arcscriptsdir = up(up(sdnadll)) 
+
+if not arcscriptsdir or not os.path.isdir(arcscriptsdir):
+    path = up(__file__)
+    arcscriptsdir = os.path.join(up(up(up(path))), "arcscripts")
+
+
 if arcscriptsdir not in sys.path:
     sys.path.insert(0,arcscriptsdir)
