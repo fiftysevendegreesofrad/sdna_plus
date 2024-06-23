0af1628f20d03d4ab225c6020d2797731a95d58f
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-06-23 15:04:41 +0100

    Update hatch_build.py

--- hatch_build.py
+++ hatch_build.py
@@ -11,9 +11,10 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 REPO_DIR = pathlib.Path(__file__).parent
 BUILD_CONFIG_CMAKE = 'Release'
 
-for __init__dot_py_dir in [REPO_DIR / 'output',
-                            REPO_DIR / 'output' / 'bin',
+for __init__dot_py_dir in [REPO_DIR / 'output' / BUILD_CONFIG_CMAKE,
+                            REPO_DIR / 'output' / BUILD_CONFIG_CMAKE / 'bin',
                             ]:
+    __init__dot_py_dir.mkdir(exist_ok = True, parents=True)
     (__init__dot_py_dir / '__init__.py').touch(exist_ok=True)
 
 
