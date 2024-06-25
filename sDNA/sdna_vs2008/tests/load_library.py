eafc54ac452cb58f4d5df27cc738030d0e6058c3
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-06-25 23:14:28 +0100

    Update load_library.py

--- sDNA/sdna_vs2008/tests/load_library.py
+++ sDNA/sdna_vs2008/tests/load_library.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import ctypes
 
@@ -5,3 +6,11 @@ if sys.platform=='win32':
     load_library = ctypes.windll.LoadLibrary
 else:
     load_library = ctypes.cdll.LoadLibrary
+
+try:
+    import sDNA.sdnapy
+except ImportError:
+    pass
+else:
+    LIB_EXT = 'dll' if ON_WINDOWS else 'so'
+    os.environ['sdnadll'] = os.path.join(os.path.dirname(sDNA.sdnapy), 'x64', 'sdna_vs2008.' + LIB_EXT)
