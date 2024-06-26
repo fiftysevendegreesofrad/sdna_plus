9c1fca21b47c614ad2eb33399dcdf86e476a3884
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-06-26 21:53:12 +0100

    Define __init__.py to import _parentdir.py

---
+++ arcscripts/__init__.py
@@ -0,0 +1,1 @@
+from .bin import _parentdir
--- hatch_build.py
+++ hatch_build.py
@@ -10,13 +10,13 @@ import shutil
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 REPO_DIR = pathlib.Path(__file__).parent
-BUILD_CONFIG_CMAKE = 'Release'
+# BUILD_CONFIG_CMAKE = 'Release'
 
-for __init__dot_py_dir in [REPO_DIR / 'output' / BUILD_CONFIG_CMAKE,
-                            REPO_DIR / 'output' / BUILD_CONFIG_CMAKE / 'bin',
-                            ]:
-    __init__dot_py_dir.mkdir(exist_ok = True, parents=True)
-    (__init__dot_py_dir / '__init__.py').touch(exist_ok=True)
+# for __init__dot_py_dir in [REPO_DIR / 'output' / BUILD_CONFIG_CMAKE,
+#                             REPO_DIR / 'output' / BUILD_CONFIG_CMAKE / 'bin',
+#                             ]:
+#     __init__dot_py_dir.mkdir(exist_ok = True, parents=True)
+#     (__init__dot_py_dir / '__init__.py').touch(exist_ok=True)
 
 
 @dataclasses.dataclass
