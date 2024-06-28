746bd3e49e96cd0c7fe14415b63975de1c4070a0
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-06-28 17:24:42 +0100

    Put requirements files in separate dir

--- create_installer_with_Visual_Studio_17_2022.bat
+++ create_installer_with_Visual_Studio_17_2022.bat
@@ -16,6 +16,6 @@ cmake -G "Visual Studio 17 2022" ^
       -S %SRC_DIR% ^
       -D USE_ZIG=OFF && ^
 cmake --build %THIS_FILE_DIR%\build_output_cmake_x64 --config %CONFIG% && ^
-python -m pip install -r requirements.txt --target %THIS_FILE_DIR%\output\%CONFIG% && ^
+python -m pip install -r %THIS_FILE_DIR%\requirements\base.txt -r %THIS_FILE_DIR%\requirements\R.txt --target %THIS_FILE_DIR%\output\%CONFIG% && ^
 AdvancedInstaller.com /build installerbits\advanced\sdna.aip && ^
 python -u installerbits\rename_version.py installerbits/advanced/output/sdna_setup.msi .
