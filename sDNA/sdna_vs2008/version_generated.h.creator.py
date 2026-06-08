dfb206b33bdcc65e8f38775d760a2cc6a428d401
James Parrott <80779630+JamesParrott@users.noreply.github.com>
2026-06-08 10:04:12 +0100

    Update version_generated.h.creator.py

--- sDNA/sdna_vs2008/version_generated.h.creator.py
+++ sDNA/sdna_vs2008/version_generated.h.creator.py
@@ -1,7 +1,7 @@
 import pathlib
 import subprocess
 
-PARENT_DIR = Path(__file__).parent
+PARENT_DIR = pathlib.Path(__file__).parent
 version_template = PARENT_DIR / 'version_template.h'
 version_generated = PARENT_DIR / 'version_generated.h'
 
@@ -11,5 +11,5 @@ template_content =  version_template.read_text()
 
 content = template_content.replace('#GITHASH#', git_hash)
 
-# Overwrites any existing file.
+# Overwrites an existing file.
 version_generated.write_text(content)
